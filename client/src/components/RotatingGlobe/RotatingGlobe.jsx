788690db6b05ba36ecba51cbbb950c35cb4fbe6f
Akshay Kadam <ak65223167@gmail.com>
2025-04-27 20:02:03 +0530

    add rotating globe component

---
+++ client/src/components/RotatingGlobe/RotatingGlobe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './RotatingGlobe.css'; // Include Earth CSS
+
+const RotatingGlobe = () => {
+  return <div id="earth"></div>;
+};
+
+export default RotatingGlobe;
+
